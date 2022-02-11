69280bbcfac1ae80bc127af157381b8bb111aa10
hoseon <hy770819@naver.com>
2022-02-11 11:51:33 +0900

    add

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    #변수 추가할 때는 python migrations 해야함
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+    # test = models.TextField()
+
+    def __str__(self):
+        return str(self.id)+' ('+self.title+')'
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
