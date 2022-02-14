6a9b1eebdaf129c743d46c5f95d6acc66dec0954
hoseon <hy770819@naver.com>
2022-02-14 14:17:21 +0900

    base template

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,23 @@
+# Create your views here.
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils import timezone
 
+from blog.models import Post
 
-# Create your views here.
 
 def post_list(request):
     my_name = '장고웹프레임워크'
     http_method = request.method
-    return HttpResponse('''
-        <h2>Welcome {name}</h2>
-        <p>Http Method : {method}</p>
-        <p>Http headers user-agent  : {header}</p>
-        <p>Http Path : {mypath}</p>
-    '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path)
-                        )
+    # return HttpResponse('''
+    #     <h2>Welcome {name}</h2>
+    #     <p>Http Method : {method}</p>
+    #     <p>Http headers user-agent  : {header}</p>
+    #     <p>Http Path : {mypath}</p>
+    # '''.format(name=my_name, method=http_method, header=request.headers['user-agent'], mypath=request.path)
+    #                     )
 
+    # return render(request, 'blog/post_list.html')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'post_list':posts})
